2f79b4d32869203bf340f83ee152f04d5c0b6614
Alencar Santos Buriti Junior <alencarburitijr@gmail.com>
2022-03-10 19:03:51 -0300

    AULA - Atualização árvore binária

--- Complexidade_Algoritmo/src/BuscaBinaria/BinarySearch.java
+++ Complexidade_Algoritmo/src/BuscaBinaria/BinarySearch.java
@@ -1,27 +1,11 @@
-
-class BinarySearch {
-    static int binarySearch(int arrayNossoDeCadaDia[], int inicio, int fim, int valor) {
-        int meio;
-        if (fim >= inicio) {
-            meio = (beg + fim) / 2;
-            if (arrayNossoDeCadaDia[meio] == valor) {
-                return meio + 1; 
-            }            
-            else if (arrayNossoDeCadaDia[meio] < valor) {
-                return binarySearch(arrayNossoDeCadaDia, meio + 1, fim, valor);
-            
-            else {
-                return binarySearch(arrayNossoDeCadaDia, inicio, meio - 1, valor);
-            }
-        }
-        return -1;
-    }
+public class BinarySearch {
+   
 
     public static void main(String args[]) {
-        int arrayNossoDeCadaDia[] = { 8, 10, 22, 27, 37, 44, 49, 55, 69 };
-        int valor = 37;
+        int arrayNossoDeCadaDia[] = { 0,1, 2, 3, 4 , 5 , 6, 7 ,8 , 9 ,10, 11 , 12, 13,14 ,15};
+        int valor = 14;
         int n = arrayNossoDeCadaDia.length;
-        int resultado = binarySearch(a, 0, n - 1, valor);
+        int resultado = binarySearch(arrayNossoDeCadaDia, 0, n - 1, valor);
         
         System.out.print("O(s) elemento do array é: ");
 
@@ -36,5 +20,24 @@ class BinarySearch {
             System.out.println("Elemento não está presente no array");
         else
             System.out.println("Elemento presente é " + resultado + " na posição do array");
+    }
+
+    static int binarySearch(int arrayNossoDeCadaDia[], int inicio, int fim, int valor) {
+        int meio;
+        if (fim >= inicio) {
+            meio = (inicio + fim) / 2;
+            if (arrayNossoDeCadaDia[meio] == valor) {
+                return meio + 1; 
+            }            
+            else if (arrayNossoDeCadaDia[meio] < valor) {
+                System.out.println("1 - Encontrou o meio" + meio);
+                return binarySearch(arrayNossoDeCadaDia, meio + 1, fim, valor);
+            }
+            else {
+                System.out.println("1 - Encontrou o meio" + meio);
+                return binarySearch(arrayNossoDeCadaDia, inicio, meio - 1, valor);
+            }
+        }
+        return -1;
     }
 }
