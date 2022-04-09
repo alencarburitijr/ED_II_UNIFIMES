e440ad83f3651e13cd5c635ceac7178d878a07cb
alencarburitijr <alencarburitijr@gmail.com>
2022-04-08 21:01:24 -0300

    AULA - Implementando grafo 08042022

--- Grafo/Vertice.java
+++ Grafo/Vertice.java
@@ -1,7 +1,30 @@
 import java.util.ArrayList;
 
 public class Vertice<TIPO> {
-    <TIPO> valor;
+    private TIPO dado;
+    private ArrayList<Aresta<TIPO>> arestasEntrada;
+    private ArrayList<Aresta<TIPO>> arestasSaida;
+
+    public Vertice(TIPO valor) {
+        this.dado = valor;
+        this.arestasEntrada = new ArrayList<Aresta<TIPO>>();
+        this.arestasSaida = new ArrayList<Aresta<TIPO>>();
+    }
+
+    public TIPO getDado(){
+        return this.dado;
+    }
+    public void setDado(TIPO valor){
+        this.dado = valor;
+    }
+
+    public void adicionarArestaEntrada(Aresta<TIPO> aresta)   {
+this.arestasEntrada.add(aresta);
+    }
+    public void adicionarArestaSaida(Aresta<TIPO> aresta)   {
+            this.arestasSaida.add(aresta);
+    }
+
     ArrayList<Vertice<TIPO>> verticeEntrada;
     ArrayList<Vertice<TIPO>> verticeSaida;
     public void adicionarVertice(ArrayList<Vertice<TIPO>> verticeEntrada){   
